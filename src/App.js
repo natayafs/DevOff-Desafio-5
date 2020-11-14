e6bc055fd2d3039da6806ae1e4ad24ab068bfe14
Nataya Soledad <natayafs@gmail.com>
2020-11-13 23:32:02 -0300

    Build app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import './App.css';
-import Home from './components/home';
-import List from './components/list';
-import Details from './components/details';
+import "./App.css";
+import Home from "./components/home";
+import List from "./components/list";
+import Details from "./components/details";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <Switch>
-            <Route exact path="/" exact component={Home} />
-            <Route path="/list" exact component={List} />
-            <Route path="/about" component={Details} />
-          </Switch>
+        <Switch>
+          <Route exact path="/" exact component={Home} />
+          <Route path="/list" exact component={List} />
+          <Route path="/about" exact component={Details} />
+        </Switch>
       </BrowserRouter>
-  </div>
+    </div>
   );
 }
 
